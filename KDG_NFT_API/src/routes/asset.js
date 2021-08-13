94d317aca861ee7d304db9f08fdb1c71fa079a6b
dung.bui <dung.bui@tpptechnology.com>
2021-08-14 05:10:21 +0700

    add get asset by id

--- KDG_NFT_API/src/routes/asset.js
+++ KDG_NFT_API/src/routes/asset.js
@@ -2,6 +2,7 @@
 
 const controller = require('../controllers/asset')
 const AssetValidation = require('../validations/asset')
+const { isAuthenticated } = require('../middlewares/auth')
 
 module.exports = router => {
   router.get('/assets/total', AssetValidation('getTotalAssets'), controller.getTotalAssets)
