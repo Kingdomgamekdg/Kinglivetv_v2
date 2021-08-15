78392268c6e28345628f53f42ecfd03598f7c4d5
dung.bui <dung.bui@tpptechnology.com>
2021-08-15 11:57:48 +0700

    order by ids

--- KDG_NFT_API/src/controllers/asset/index.js
+++ KDG_NFT_API/src/controllers/asset/index.js
@@ -2,6 +2,22 @@
 
 const AssetService = require('../../services/asset')
 
+function mapOrder (array, order, key) {
+  
+  array.sort( function (a, b) {
+    var A = a[key], B = b[key];
+    
+    if (order.indexOf(A) > order.indexOf(B)) {
+      return 1;
+    } else {
+      return -1;
+    }
+    
+  });
+  
+  return array;
+};
+
 module.exports = class {
   static async getTotalAssets (_req, _res) {
     try {
@@ -29,13 +45,16 @@ module.exports = class {
       const queries = _req.query
       const ids = queries.ids ? queries.ids.split(',') : []
 
-      const asset = await AssetService.find({_id : {$in : ids}}).populate({
+      const assets = await AssetService.find({_id : {$in : ids}}).populate({
         path : 'owner',
       })
 
+      ordered_array = mapOrder(assets, item_order, '_id');
+
+
       _res.status(200).json({
         status:1,
-        data: asset
+        data: ordered_array
       })
     } catch (e) {
       _res.status(400).json(e.message)
