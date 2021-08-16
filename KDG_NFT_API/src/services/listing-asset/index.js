ddc1b95f85e4158b189368b06360375140ab4e7a
BrianNguyen <brian@bigbom.com>
2021-08-16 14:52:25 +0700

    update(Api): update avatar for top9 and populate api

--- KDG_NFT_API/src/services/listing-asset/index.js
+++ KDG_NFT_API/src/services/listing-asset/index.js
@@ -1,11 +1,10 @@
 'use strict'
 const BaseService = require('../../cores/base-service')
 const Model = require('../../models/ListingAsset')
-const UploadsService = require('../../services/upload')
 
 class ListingAssetsService extends BaseService {
   async getListingAsset ({ ids }, limit) {
-    let data = await this
+    const data = await this
       .find({
         _id: { $nin: ids },
         quantity: { $gt: 0 }
@@ -13,47 +12,70 @@ class ListingAssetsService extends BaseService {
       .sort({ quantity: 1 })
       .limit(limit)
       .populate({
-        path: 'asset owner'
+        path: 'asset owner',
+        populate: {
+          path: 'kyc.avatar uploads'
+        }
       })
       .populate({
         path: 'buys',
-        populate: 'from to'
+        populate: {
+          path: 'from to',
+          populate: {
+            path: 'kyc.avatar uploads'
+          }
+        }
       })
       .populate({
         path: 'bid_orders',
-        populate: 'from to'
+        populate: {
+          path: 'from to',
+          populate: {
+            path: 'kyc.avatar uploads'
+          }
+        }
       })
       .lean()
     data.reverse()
 
-    if (data.length) {
-      data = await UploadsService.mappingAvatar({ data, key: 'owner' })
-    }
     return data
   }
 
   async getTopSellAssets (conditions, limit) {
-    let data = await this
+    const data = await this
       .find(conditions)
       .sort({ quantity: 1 })
       .limit(limit)
       .populate({
-        path: 'asset owner'
+        path: 'asset owner',
+        populate: {
+          path: 'kyc.avatar uploads'
+        }
       })
       .populate({
         path: 'buys',
-        populate: 'from to'
+        populate: {
+          path: 'from to',
+          populate: {
+            path: 'kyc.avatar uploads'
+          }
+        }
       })
       .populate({
         path: 'bid_orders',
-        populate: 'from to'
+        populate: {
+          path: 'from to',
+          populate: {
+            path: 'kyc.avatar uploads'
+          }
+        }
       })
       .lean()
     data.reverse()
-
-    if (data.length) {
-      data = await UploadsService.mappingAvatar({ data, key: 'owner' })
-    }
+    //
+    // if (data.length) {
+    //   data = await UploadsService.mappingAvatar({ data, key: 'owner' })
+    // }
     return data
   }
 }
