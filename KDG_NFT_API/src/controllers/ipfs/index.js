2895b7baef1433baf2cee1d4434f971f47043cea
dung.bui <dung.bui@tpptechnology.com>
2021-08-21 08:52:32 +0700

    check bug gifl

--- KDG_NFT_API/src/controllers/ipfs/index.js
+++ KDG_NFT_API/src/controllers/ipfs/index.js
@@ -69,6 +69,78 @@ class Controller {
    * Uploads metadata and file, image to IPFS
    */
     static async uploadMetadata (_req, _res) {
+      try {
+        const { name, description, image, file } = _req.body
+        console.log('_req.body', _req.body)
+        const content = {
+          name,
+          description
+        }
+  
+        let preview
+        let thumbnail
+  
+        if (file) {
+          const hashes = await _uploadFile(file, image)
+          console.log('hashes', hashes)
+          if (file.mimetype.startsWith('image')) {
+            content.image = `${config.IPFS_GATEWAY}/${hashes[0]}`
+  
+            preview = `${config.IPFS_GATEWAY}/${hashes[1]}`
+            thumbnail = `${config.IPFS_GATEWAY}/${hashes[2]}`
+          } else {
+            content.animation_url = `${config.IPFS_GATEWAY}/${hashes[0]}`
+            content.image = `${config.IPFS_GATEWAY}/${hashes[1]}`
+  
+            preview = `${config.IPFS_GATEWAY}/${hashes[2]}`
+            thumbnail = `${config.IPFS_GATEWAY}/${hashes[3]}`
+          }
+  
+          content.mimetype = file.mimetype
+        }
+  
+        const data = []
+        const metadataList = []
+  
+        // Creates metadata to upload ipfs
+        // for (let i = 1; i <= numEditions; i++) {
+        const metadata = { ...content }
+  
+        data.push({
+          path: '/',
+          content: Buffer.from(JSON.stringify(metadata))
+        })
+  
+        metadata.image_preview = preview
+        metadata.image_thumbnail = thumbnail
+        console.log('metadata', metadata)
+        metadataList.push({ metadata })
+        // }
+  
+        // Uploads metadata to IPFS
+        const hashes = await ipfs.addFiles(data)
+  
+        if (hashes == null) {
+          throw new HttpError(500)
+        }
+  
+        // Creates metadata to save database
+        for (let i = 0; i < hashes.length; i++) {
+          metadataList[i].uri = hashes[i]
+        }
+        console.log('metadataList', metadataList)
+        // await Controller._saveCollectionMetadata(metadataList);
+        if (metadataList.length) {
+          await AssetMetadataService.create({
+            uri: metadataList[0]?.uri,
+            metadata: metadataList[0]?.metadata
+          })
+        }
+        response.success(_res, { hashes })
+      } catch(ex){
+        console.log("ex",ex)
+        throw new HttpError(500)
+      }
       // console.log("_req",_req);
       // const params = common.validateInputParams(_req.body, joi.object().keys({
       //     // type: joi.string().valid(['collection', 'asset']).insensitive().default('collection'),
@@ -79,73 +151,7 @@ class Controller {
       //     numEditions: joi.number().optional().greater(0).default(1),
       //     // blankMode: joi.object().optional()
       // }));
-      const { name, description, image, file } = _req.body
-      console.log('_req.body', _req.body)
-      const content = {
-        name,
-        description
-      }
-
-      let preview
-      let thumbnail
-
-      if (file) {
-        const hashes = await _uploadFile(file, image)
-        console.log('hashes', hashes)
-        if (file.mimetype.startsWith('image')) {
-          content.image = `${config.IPFS_GATEWAY}/${hashes[0]}`
-
-          preview = `${config.IPFS_GATEWAY}/${hashes[1]}`
-          thumbnail = `${config.IPFS_GATEWAY}/${hashes[2]}`
-        } else {
-          content.animation_url = `${config.IPFS_GATEWAY}/${hashes[0]}`
-          content.image = `${config.IPFS_GATEWAY}/${hashes[1]}`
-
-          preview = `${config.IPFS_GATEWAY}/${hashes[2]}`
-          thumbnail = `${config.IPFS_GATEWAY}/${hashes[3]}`
-        }
-
-        content.mimetype = file.mimetype
-      }
-
-      const data = []
-      const metadataList = []
-
-      // Creates metadata to upload ipfs
-      // for (let i = 1; i <= numEditions; i++) {
-      const metadata = { ...content }
-
-      data.push({
-        path: '/',
-        content: Buffer.from(JSON.stringify(metadata))
-      })
-
-      metadata.image_preview = preview
-      metadata.image_thumbnail = thumbnail
-      console.log('metadata', metadata)
-      metadataList.push({ metadata })
-      // }
-
-      // Uploads metadata to IPFS
-      const hashes = await ipfs.addFiles(data)
-
-      if (hashes == null) {
-        throw new HttpError(500)
-      }
-
-      // Creates metadata to save database
-      for (let i = 0; i < hashes.length; i++) {
-        metadataList[i].uri = hashes[i]
-      }
-      console.log('metadataList', metadataList)
-      // await Controller._saveCollectionMetadata(metadataList);
-      if (metadataList.length) {
-        await AssetMetadataService.create({
-          uri: metadataList[0]?.uri,
-          metadata: metadataList[0]?.metadata
-        })
-      }
-      response.success(_res, { hashes })
+      
     }
 
     /**
